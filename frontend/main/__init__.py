5429a0d0b5c6abf12ee17ef63e82c7731e2e9746
j <j>
2022-11-21 14:06:24 -0300

    working on templates

--- frontend/main/__init__.py
+++ frontend/main/__init__.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Method for initialize all modules and will return the app
 def create_app():
     # Initialize Flask
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
 
     # Load environment  variables
     load_dotenv()
@@ -13,6 +13,12 @@ def create_app():
     #
     # Here will be initialized the rest of the app modules
     #
+    app.config['API_URL'] = os.getenv('API_URL')
+
+    from main.routes import user, poem, main
+    app.register_blueprint(main.main)
+    app.register_blueprint(user.user)
+    app.register_blueprint(poem.poem)
 
     # Return initialized app
     return app
